c8ab82322bcf3b8645ff8f5709a3de9249fcd5ea
22dm <22dm@163.com>
2014-08-31 21:49:48 +0800

    first post

---
+++ lhy's blog   又一个WordPress站点_files/script.js
@@ -0,0 +1,58 @@
+<<<<<<< HEAD
+// Ctrl + Enter 提交表单
+function cmt_submit(){
+	if(event.ctrlKey && window.event.keyCode==13) {
+		document.getElementById("cmt_form").submit.click();
+	};
+};
+
+// 代码可编辑
+function clrs_code(){
+	var controls = document.getElementsByTagName('pre');
+	for(var i=0; i<controls.length; i++){
+		controls[i].spellcheck = false;
+		controls[i].setAttribute("contenteditable","true")
+	};
+	var controls = document.getElementsByTagName('code');
+	for(var i=0; i<controls.length; i++){
+		controls[i].spellcheck = false;
+		controls[i].setAttribute("contenteditable","true");
+	};
+}
+
+document.onkeydown = cmt_submit;
+window.onload = clrs_code;
+
+console.log('Theme Clearision by Dimpurr')
+console.log('http://dimpurr.com')
+console.log('')
+=======
+// Ctrl + Enter 提交表单
+function cmt_submit(){
+	if(event.ctrlKey && window.event.keyCode==13) {
+		document.getElementById("cmt_form").submit.click();
+	};
+};
+
+// 代码可编辑
+function clrs_code(){
+	var controls = document.getElementsByTagName('pre');
+	for(var i=0; i<controls.length; i++){
+		controls[i].spellcheck = false;
+		controls[i].setAttribute("contenteditable","true")
+	};
+	var controls = document.getElementsByTagName('code');
+	for(var i=0; i<controls.length; i++){
+		controls[i].spellcheck = false;
+		controls[i].setAttribute("contenteditable","true");
+	};
+}
+
+document.onkeydown = cmt_submit;
+window.onload = clrs_code;
+
+console.log('Theme Clearision by Dimpurr')
+console.log('http://dimpurr.com')
+console.log('')
+>>>>>>> b2288136341fc462677ae1749d958aee0c97a559
+console.log('Welcome to my blog!')
