d32bd2e3b6ac8f7c4bed445d9982a27fefc874db
lhy <22dm@163.com>
2014-12-14 14:21:19 +0800

    push

---
+++ adr/lang/langs.js
@@ -0,0 +1,16 @@
+var langs = {
+	'de':'deutsch',
+	'en':'english',
+	'es':'español',
+	'fr':'français',
+	'it':'italiano',
+	'ja':'日本',
+	'ko':'한국어',
+	'pl':'polski',
+	'pt':'português',
+	'ru':'русский',
+	'sv':'svenska',
+	'tr':'türkçe',
+	'uk':'українська',
+	'zh_cn':'简体中文'
+};
